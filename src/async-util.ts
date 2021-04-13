c140316e06125871c30b65a1e5adbe06bfb87d34
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2021-04-13 17:00:33 +0200

    feat: async timeout

--- src/async-util.ts
+++ src/async-util.ts
@@ -8,11 +8,10 @@ export const asyncUtil = {
   httpErrorHandler: (_target: any, _key: string, descriptor: TypedPropertyDescriptor<any>): any => {
     const originalMethod = descriptor.value
     descriptor.value = function (): any {
-      // eslint-disable-next-line prefer-rest-params
-      const next = arguments[2]
-      // eslint-disable-next-line prefer-rest-params
-      return Promise.resolve(originalMethod.apply(this, arguments)).catch(next)
+      const next = arguments[2] // eslint-disable-line prefer-rest-params
+      return Promise.resolve(originalMethod.apply(this, arguments)).catch(next) // eslint-disable-line prefer-rest-params
     }
     return descriptor
   },
+  timeout: (t: number): Promise<NodeJS.Timeout> => new Promise((resolve) => setTimeout(resolve, t)),
 }
