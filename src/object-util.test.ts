66b9bce431ea81243a14ba41456df0f831878852
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2022-01-04 11:59:39 +0100

    feat!: object prop null->undefined (deep)
    BREAKING CHANGE: rename stringifyOrNullUndefined -> stringifySortOrNullOrUndefined

---
+++ src/object-util.test.ts
@@ -0,0 +1,106 @@
+import { objectUtil } from './object-util'
+
+describe('objectUtil', () => {
+  const everyType = {
+    number: 1,
+    decimal: 1.12345,
+    string: 'string',
+    undefined: undefined,
+    notANumber: NaN,
+    emptyObj: {},
+    date: new Date('2020-01-01'),
+    boolean: true,
+    nestedObject: { obj: 'test' },
+  }
+
+  describe('deepClone', () => {
+    it.each([
+      [{ test: 'string' }],
+      [{ deep: { test: 'string' } }],
+      [{ deeper: { deep: { test: 'string' } } }],
+      [everyType],
+      [{ deep: everyType }],
+      [{ deeper: { deep: everyType } }],
+    ])('should clone %j', (obj) => {
+      expect(objectUtil.deepClone(obj)).not.toBe(obj)
+    })
+  })
+
+  describe('deepNullToUndefined', () => {
+    it.each([
+      [{ test: undefined }, { test: undefined }],
+      [{ test: null }, { test: undefined }],
+      [{ deep: { test: null } }, { deep: { test: undefined } }],
+      [{ deeper: { deep: { test: null } } }, { deeper: { deep: { test: undefined } } }],
+      [everyType, everyType],
+      [{ deep: everyType }, { deep: everyType }],
+      [{ deeper: { deep: everyType } }, { deeper: { deep: everyType } }],
+    ])('should convert null to undefined for %j', (withNulls, withUndefined) => {
+      expect(objectUtil.deepNullToUndefined(withNulls)).toEqual(withUndefined)
+    })
+  })
+
+  describe('stringifySortOrNullOrUndefined', () => {
+    it.each([
+      [null, null],
+      [undefined, undefined],
+      [{}, '{}'],
+      [
+        everyType,
+        `{
+  boolean: true,
+  date: 2020-01-01T00:00:00.000Z,
+  decimal: 1.12345,
+  emptyObj: {},
+  nestedObject: {
+    obj: 'test'
+  },
+  notANumber: NaN,
+  number: 1,
+  string: 'string',
+  undefined: undefined
+}`,
+      ],
+      [
+        { deep: everyType },
+        `{
+  deep: {
+    boolean: true,
+    date: 2020-01-01T00:00:00.000Z,
+    decimal: 1.12345,
+    emptyObj: {},
+    nestedObject: {
+      obj: 'test'
+    },
+    notANumber: NaN,
+    number: 1,
+    string: 'string',
+    undefined: undefined
+  }
+}`,
+      ],
+      [
+        { deeper: { deep: everyType } },
+        `{
+  deeper: {
+    deep: {
+      boolean: true,
+      date: 2020-01-01T00:00:00.000Z,
+      decimal: 1.12345,
+      emptyObj: {},
+      nestedObject: {
+        obj: 'test'
+      },
+      notANumber: NaN,
+      number: 1,
+      string: 'string',
+      undefined: undefined
+    }
+  }
+}`,
+      ],
+    ])('should compare %j with result %j', (value, result) => {
+      expect(objectUtil.stringifySortOrNullOrUndefined(value)).toEqual(result)
+    })
+  })
+})
--- src/object-util.ts
+++ src/object-util.ts
@@ -1,13 +1,30 @@
 import cloneDeep from 'lodash.clonedeep'
+import util from 'util'
 
 export type ObjectType = { [k: string]: any }
 
 export const objectUtil = {
-  stringifyOrNullUndefined: (obj?: ObjectType | null): string | null | undefined => {
+  stringifySortOrNullOrUndefined: (obj?: ObjectType | null): string | null | undefined => {
     if (obj == null) return obj
-    return JSON.stringify(obj, Object.keys(obj).sort())
+    return util.inspect(obj, {
+      depth: Infinity,
+      sorted: true,
+      maxArrayLength: Infinity,
+      maxStringLength: Infinity,
+      compact: 0,
+      breakLength: Infinity,
+    })
   },
   deepClone: <T extends ObjectType>(objectToClone: T): T => {
     return cloneDeep(objectToClone)
+  },
+  deepNullToUndefined: <T extends ObjectType>(objectWithNulls: T): T => {
+    return Object.entries(objectWithNulls).reduce((acc, cur) => {
+      const [key, value] = cur
+      if (value === null) acc[key] = undefined
+      else if (typeof value === 'object' && !(value instanceof Date)) acc[key] = objectUtil.deepNullToUndefined(value)
+      else acc[key] = value
+      return acc
+    }, {} as any)
   },
 }
