b12be30c2c3ab392b720ae5853edaf47d7126564
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2022-02-06 15:37:45 +0100

    feat: update typeUtil, exhaustive check
    split into two checks message/error

--- src/type-util.ts
+++ src/type-util.ts
@@ -1,5 +1,20 @@
-export const typeUtil = {
+const _self = {
+  exhaustiveMessage: (message: string, _: never): string => {
+    return `${message}`
+  },
+  exhaustiveError: (message: string, _: never): Error => {
+    return new Error(_self.exhaustiveMessage(message, _))
+  },
+  /**
+   * @deprecated since version 3.2.0
+   * Please use exhaustiveError
+   * @param {string} message
+   * @param {never} _
+   * @returns {Error}
+   */
   exhaustiveCheck: (message: string, _: never): Error => {
     return new Error(`ExhaustiveCheck: ${message} [${_}]`)
   },
 }
+
+export const typeUtil = _self
