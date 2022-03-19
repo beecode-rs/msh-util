5e8bdda7a503b2f2cb89e418b845363444b4dbcd
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2022-03-19 18:58:35 +0100

    feat: add string and update object utils

--- src/object-util.ts
+++ src/object-util.ts
@@ -27,4 +27,24 @@ export const objectUtil = {
       return acc
     }, {} as any)
   },
+  deepEqual: (a: any, b: any): boolean => {
+    return JSON.stringify(a) === JSON.stringify(b)
+  },
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  pick: <P extends T, T extends object>(entity: T, model: P): P => {
+    const keys = Object.keys(model)
+    return objectUtil.pickByList<P, T>(entity, keys)
+  },
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  pickByList: <P extends T, T extends object>(entity: T, propertyList: string[]): P => {
+    return propertyList.reduce((obj, key) => {
+      // eslint-disable-next-line
+      if (entity && entity.hasOwnProperty(key)) {
+        // eslint-disable-next-line
+        // @ts-ignore
+        obj[key] = entity[key]
+      }
+      return obj
+    }, {} as P)
+  },
 }
