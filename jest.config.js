369ea2ce7ab9fdb430010ae39727ca5055532166
milosbugarinovic <milos.bugarinovic@gmail.com>
2023-04-04 19:14:45 +0200

    fix: update eslint/prettier and update package.json

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/**/*.{contract,d}.ts'],
+	coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/', '/__mocks__/'],
+	maxConcurrency: 1,
+	moduleNameMapper: {
+		'^src/(.*)$': '<rootDir>/src/$1',
+	},
+	moduleFileExtensions: ['js', 'ts'],
+	preset: 'ts-jest',
+	roots: ['<rootDir>/src'],
+	setupFilesAfterEnv: ['jest-extended/all'],
+	testEnvironment: 'node',
+	testMatch: ['<rootDir>/src/**/*.test.ts'],
+	testPathIgnorePatterns: ['/node_modules/'],
+}
