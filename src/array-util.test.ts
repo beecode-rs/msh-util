fdb6a4484afa6faa2e0e3c3a581515d59204f64f
milosbugarinovic <milos.bugarinovic@gmail.com>
2023-04-04 19:02:41 +0200

    feat: add array util

---
+++ src/array-util.test.ts
@@ -0,0 +1,24 @@
+import { arrayUtil } from 'src/array-util'
+
+describe('arrayUtil', () => {
+	describe('notEmpty', () => {
+		it.each([
+			[
+				[1, 2, undefined, 4, null, 5],
+				[1, 2, 4, 5],
+			],
+			[[undefined, null], []],
+			[[undefined, 4, null], [4]],
+			[
+				[[1], [2], [undefined], [4], [null], [5]],
+				[[1], [2], [undefined], [4], [null], [5]],
+			],
+			[
+				[{ a: 1 }, { a: 2 }, { a: undefined }, { a: 4 }, { a: null }, { a: 5 }],
+				[{ a: 1 }, { a: 2 }, { a: undefined }, { a: 4 }, { a: null }, { a: 5 }],
+			],
+		])('%#. should filter not empty values from array %s to be %s', (arr: any, expected) => {
+			expect(arr.filter(arrayUtil.notEmpty)).toEqual(expected)
+		})
+	})
+})
