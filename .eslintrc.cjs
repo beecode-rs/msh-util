c8e1cf27c7bfb11d375f6fc3002b9d684da9efe1
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-04-13 09:47:18 +0200

    chore: update eslint rule no floating promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -46,6 +46,7 @@ module.exports = {
 	},
 	plugins: ['@typescript-eslint', 'eslint-plugin-import', 'import', 'no-only-tests', 'no-loops', 'jest', 'sort-keys-fix'],
 	rules: {
+		'@typescript-eslint/no-floating-promises': ['error'],
 		// ESLINT
 		// 'no-restricted-imports': [
 		// 	'error',
